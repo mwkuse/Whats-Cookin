26b9b7876b2a3976ba69235789b8d16c2db98510
bserrato7900 <baserrato@mail.csuchico.edu>
2022-03-14 21:12:56 -0700

    Creating website html pages for WhatsCookin and added a start model design

--- WhatsCookin/Popular/views.py
+++ WhatsCookin/Popular/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+    title = "What's Cookin"
+    content = "Hello Team"
+    context = {
+        "title":title,
+        "body":content
+    }
+    return render(request, "index.html", context = context)
