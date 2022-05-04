a58f6ca50105293b2e07f3b70db9287814294936
WCHunt <wchunt@mail.csuchico.edu>
2022-05-04 13:10:28 -0700

    updated csv

--- WhatsCookin/Random/views.py
+++ WhatsCookin/Random/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect, HttpResponse
+from Home.models import Recipe
+import re
 # Create your views here.
 
 def Random(request):
-    return render(request,"Random/Random.html")
+    recipes = Recipe.objects.all()
+    ingredient = []
+    for i in recipes:
+        for j in i.ingredients:
+            print(j.split('"'))
+    context = {
+        'Recipes' : recipes,
+        'ingredient' : ingredient,
+    }
+    return render(request,"Random/Random.html", context)
