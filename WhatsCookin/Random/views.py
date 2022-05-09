d69cfcb5119d871fea1b148b6c6dc7daf1a44260
WCHunt <wchunt@mail.csuchico.edu>
2022-05-09 11:14:17 -0700

    made changes to random views.py, that allows for 25 random recipes to be displayed when clicked. Also updated random.html to display these random recipes.

--- WhatsCookin/Random/views.py
+++ WhatsCookin/Random/views.py
@@ -2,16 +2,32 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect, HttpResponse
 from Home.models import Recipe
 import re
+from random import *
 # Create your views here.
 
 def Random(request):
     recipes = Recipe.objects.all()
-    ingredient = []
-    for i in recipes:
-        for j in i.ingredients:
-            print(j.split('"'))
+    #print(recipes.count())
+    count = 0
+    selected = []
+    while count < 25:
+        id = randrange(recipes.count())
+        selected.append(Recipe.objects.get(id=id))
+        count+=1
+
+    #code below for us in pantry.
+    #ingredient = []
+    #for i in recipes:
+    #    for j in i.ingredients:
+    #        j = j.replace("'","")
+    #        j = j.lstrip()
+    #        j = j.rstrip()
+    #        print(j)
+    #        if j not in ingredient:
+    #            ingredient.append(str(j))
+    #            print(len(ingredient))
     context = {
         'Recipes' : recipes,
-        'ingredient' : ingredient,
+        'Selected' : selected,
     }
     return render(request,"Random/Random.html", context)
