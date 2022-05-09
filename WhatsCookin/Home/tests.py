2b0a97430bdabbe7d17dd6021dc0e34bda8cfa1a
WCHunt <wchunt@mail.csuchico.edu>
2022-05-09 13:27:38 -0700

    Updated recipe model to be saved.

--- WhatsCookin/Home/tests.py
+++ WhatsCookin/Home/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
-
+from Home.models import Recipe
 # Create your tests here.
+
+#class RecipeTestCase(TestCase):
+#    create test objects
+
+
+#   test if objects exist
+
+
+# test if object fails
